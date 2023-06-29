18793a7a2c48e667740fcb3f323b9ba55a883c06
K4-R4 <mabdman512@gmail.com>
2023-06-29 19:32:33 +0900

    norm

--- client.c
+++ client.c
@@ -1,69 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 19:19:49 by tkuramot          #+#    #+#             */
+/*   Updated: 2023/06/29 19:31:55 by tkuramot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-static int received;
+static int	g_received;
 
-static void    send_char(pid_t pid, unsigned char  c)
+static void		send_char(pid_t pid, unsigned char  c)
 {
-    unsigned char   mask;
+	unsigned char	mask;
 
-    mask = 0b10000000;
-    while (mask)
-    {
-        usleep(50);
-        if (mask & c)
-        {
-            if (kill(pid, SIGUSR1) < 0)
-                return;
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) < 0)
-                return;
-        }
-        mask >>= 1;
-        while (!(received == BIT_RECEIVED))
-            pause();
+	mask = 0b10000000;
+	while (mask)
+	{
 		usleep(50);
-    }
+		if (mask & c)
+		{
+			if (kill(pid, SIGUSR1) < 0)
+				return ;
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) < 0)
+				return ;
+		}
+		mask >>= 1;
+		while (!(g_received == BIT_RECEIVED))
+			pause();
+		usleep(50);
+	}
 }
 
-static void send_str(pid_t pid, char *c)
+static void		send_str(pid_t pid, char *c)
 {
-    size_t  index;
-    size_t  len;
+	size_t	index;
+	size_t	len;
 
-    index = 0;
-    len = ft_strlen(c);
-    while (index < len)
+	index = 0;
+	len = ft_strlen(c);
+	while (index < len)
 	{
-        send_char(pid, c[index]);
+		send_char(pid, c[index]);
 		index++;
 	}
 }
 
-static void handler(int signal, siginfo_t *server, void *con)
+static void		handler(int signal, siginfo_t *server, void *con)
 {
-    (void)server;
-    (void)con;
-    if (signal == SIGUSR1)
-        received = BIT_RECEIVED;
+	(void)server;
+	(void)con;
+	if (signal == SIGUSR1)
+		g_received = BIT_RECEIVED;
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    struct sigaction    act;
+	struct sigaction	act;
 
-    if (argc != 3)
-    {
-        ft_printf("Usage: ./client {pid} {message}\n");
-        return (0);
-    }
-    ft_bzero(&act, sizeof(struct sigaction));
-    act.sa_sigaction = handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
-    send_str((pid_t)ft_atoi(argv[1]), argv[2]);
-    return 0;
+	if (argc != 3)
+	{
+		ft_printf("Usage: ./client {pid} {message}\n");
+		return (0);
+	}
+	ft_bzero(&act, sizeof(struct sigaction));
+	act.sa_sigaction = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	send_str((pid_t)ft_atoi(argv[1]), argv[2]);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,37 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 19:23:02 by tkuramot          #+#    #+#             */
+/*   Updated: 2023/06/29 19:29:38 by tkuramot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/include/ft_printf.h"
 #include "minitalk.h"
 #include <unistd.h>
 
-void handler(int sig, siginfo_t *client, void *con)
+void	handler(int sig, siginfo_t *client, void *con)
 {
-    static unsigned char mask = 0b10000000;
-    static unsigned char c;
+	static unsigned char	mask = 0b10000000;
+	static unsigned char	c;
 
-    (void)con;
-    if (sig == SIGUSR1)
-        c |= mask;
-    mask >>= 1;
-    if (mask <= 0 && c)
-    {
-        ft_putchar_fd(c, STDOUT_FILENO);
-        mask = 0b10000000;
-        c = 0;
-    }
-    kill(client->si_pid, SIGUSR1);
+	(void)con;
+	if (sig == SIGUSR1)
+		c |= mask;
+	mask >>= 1;
+	if (mask <= 0 && c)
+	{
+		ft_putchar_fd(c, STDOUT_FILENO);
+		mask = 0b10000000;
+		c = 0;
+	}
+	kill(client->si_pid, SIGUSR1);
 }
 
-int main()
+int		main(void)
 {
-    struct sigaction    act;
+	struct sigaction	act;
 
 	ft_printf("PID: %d\n", getpid());
-    ft_bzero(&act, sizeof(struct sigaction));
-    act.sa_sigaction = handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &act, NULL);
-    sigaction(SIGUSR2, &act, NULL);
-	while(1)
+	ft_bzero(&act, sizeof(struct sigaction));
+	act.sa_sigaction = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	while (1)
 		;
-    return 0;
+	return (0);
 }
