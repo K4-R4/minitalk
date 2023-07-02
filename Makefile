1d760a3806fc58e4bbe1852e9f183f651fba0a7c
K4-R4 <mabdman512@gmail.com>
2023-07-02 13:43:17 +0900

    bonus

--- Makefile
+++ Makefile
@@ -4,12 +4,20 @@ SERVER = server
 CLIENT = client
 INCLUDE = -I $(LIBFT_DIR)
 LDFLAGS = -L $(LIBFT_DIR) -l $(LIBFT)
-SERVER_SRC = server.c
-SERVER_OBJ = $(SERVER_SRC:.c=.o)
-CLIENT_SRC = client.c
-CLIENT_OBJ = $(CLIENT_SRC:.c=.o)
+SERVER_MSRC = server.c
+CLIENT_MSRC = client.c
+SERVER_BSRC = server_bonus.c
+CLIENT_BSRC = client_bonus.c
 LIBFT_DIR = libft
 LIBFT = ft
+
+ifdef is_bonus
+	SERVER_OBJ = $(SERVER_BSRC:.c=.o)
+	CLIENT_OBJ = $(CLIENT_BSRC:.c=.o)
+else
+	SERVER_OBJ = $(SERVER_MSRC:.c=.o)
+	CLIENT_OBJ = $(CLIENT_MSRC:.c=.o)
+endif
 
 all: $(NAME)
 
@@ -24,12 +32,15 @@ $(SERVER): $(SERVER_OBJ)
 $(CLIENT): $(CLIENT_OBJ)
 	$(CC) $(CFLAGS) $(INCLUDE) $(LDFLAGS) $(CLIENT_OBJ) -o $@
 
+bonus:
+	$(MAKE) is_bonus=1
+
 .c.o:
 	$(CC) $(CFLAGS) $(INCLUDE) -c $< -o $@
 
 clean:
 	$(MAKE) -C $(LIBFT_DIR) clean
-	$(RM) $(SERVER_OBJ) $(CLIENT_OBJ)
+	$(RM) $(SERVER_MSRC:.c=.o) $(SERVER_BSRC:.c=.o) $(CLIENT_MSRC:.c=.o) $(CLIENT_BSRC:.c=.o)
 
 fclean: clean
 	make -C $(LIBFT_DIR) fclean
---
+++ server_bonus.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkuramot <tkuramot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 19:23:02 by tkuramot          #+#    #+#             */
+/*   Updated: 2023/07/02 13:14:32 by tkuramot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/include/ft_printf.h"
+#include "minitalk.h"
+#include <signal.h>
+#include <sys/signal.h>
+#include <unistd.h>
+
+void	handler(int sig, siginfo_t *client, void *con)
+{
+	static unsigned char	mask = 0b10000000;
+	static unsigned char	c;
+
+	(void)con;
+	if (sig == SIGUSR1 || sig == SIGUSR2)
+	{
+		if (sig == SIGUSR1)
+			c |= mask;
+			mask >>= 1;
+		if (mask <= 0 && c)
+		{
+			ft_putchar_fd(c, STDOUT_FILENO);
+			mask = 0b10000000;
+			c = 0;
+		}
+			kill(client->si_pid, SIGUSR1);
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	act;
+
+	ft_printf("PID: %d\n", getpid());
+	ft_bzero(&act, sizeof(struct sigaction));
+	act.sa_sigaction = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	while (1)
+		;
+	return (0);
+}
